6b4bd935d9874d5f555a042c46e6200ece82653f
gdiazs <diazgbs@gmail.com>
2019-02-06 19:33:05 -0600

    db dump

--- microservices/accounts/accounts-web/src/main/resources/db/initDB.sql
+++ microservices/accounts/accounts-web/src/main/resources/db/initDB.sql
@@ -1,27 +1,27 @@
-CREATE TABLE ACCOUNTS (
-	ACCOUNT_NUMBER VARCHAR(100),
-	ACCOUNT_ALIAS VARCHAR(200) NOT NULL,
-	ACCOUNT_BALANCE DECIMAL(12, 5) NOT NULL default 0,
-	ACCOUNT_CURRENCY VARCHAR(3) NOT NULL,
-	ACCOUNT_ENABLED INTEGER NOT NULL default 1,
-	USER_ID	VARCHAR(100) NOT NULL,
-	ACCOUNT_VERSION INTEGER NOT NULL default 1,
-	PRIMARY KEY(ACCOUNT_NUMBER, USER_ID)
+-- Drop table
+
+-- DROP TABLE public.accounts
+
+CREATE TABLE public.accounts (
+	account_number varchar(100) NOT NULL,
+	account_alias varchar(200) NOT NULL,
+	account_balance numeric(12,5) NOT NULL DEFAULT 0,
+	account_currency varchar(3) NOT NULL,
+	account_enabled int4 NOT NULL DEFAULT 1,
+	user_id varchar(100) NOT NULL,
+	account_version int4 NOT NULL DEFAULT 1,
+	CONSTRAINT accounts_pkey PRIMARY KEY (account_number, user_id)
 );
 
 INSERT INTO public.accounts
 (account_number, account_alias, account_balance, account_currency, account_enabled, user_id, account_version)
-VALUES('CR05015202001026284066', 'Cuenta Corriente', 10000.00, 'CRC', 1, 'gdiazs', 1);
-
+VALUES('CR05015202001026284066', 'Cuenta Corriente', 10000.00000, 'CRC', 1, 'gdiazs', 1);
 INSERT INTO public.accounts
 (account_number, account_alias, account_balance, account_currency, account_enabled, user_id, account_version)
-VALUES('CR05015202001026284067', 'Cuenta Planilla', 10000.00, 'CRC', 1, 'gdiazs', 1);
-
+VALUES('CR05015202001026284067', 'Cuenta Planilla', 10000.00000, 'CRC', 1, 'gdiazs', 1);
 INSERT INTO public.accounts
 (account_number, account_alias, account_balance, account_currency, account_enabled, user_id, account_version)
-VALUES('CR05015202001056284067', 'Cuenta Ahorros', 5000.00, 'USD', 1, 'gdiazs', 1);
-
+VALUES('CR05015202001056284067', 'Cuenta Ahorros', 5000.00000, 'USD', 1, 'gdiazs', 1);
 INSERT INTO public.accounts
 (account_number, account_alias, account_balance, account_currency, account_enabled, user_id, account_version)
-VALUES('CR05015302001056284067', 'Saving account', 1200.00, 'USD', 1, 'macme', 1);
-
+VALUES('CR05015302001056284067', 'Saving account', 1200.00000, 'USD', 1, 'macme', 1);
